0458af8dec9e5a5ccfa4c01b9b9524bd42cf9a2c
David Leon <leoc@unicauca.edu.co>
2019-07-13 13:52:02 -0700

    My first commit in Xcode.

---
+++ AmericaCup/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AmericaCup
+//
+//  Created by David Leon on 7/12/19.
+//  Copyright © 2019 David Leon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
