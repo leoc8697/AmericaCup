ede2956190bc9cc622dc2225997eceb6be511501
David Leon <leoc@unicauca.edu.co>
2019-07-23 00:48:19 -0500

    Example Muy implemente but App do not run.

--- AmericaCup/FirstViewController.swift
+++ AmericaCup/FirstViewController.swift
@@ -8,128 +8,88 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    @IBOutlet weak var labelHeader: UILabel!
-    @IBOutlet weak var tableView: UITableView!
-       
-    var movieList = [MatchData]()
+class FirstViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate,URLSessionDelegate {
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet var collectionViewMain: UICollectionView!
+        //@IBOutlet var floatButtonScanner: UIButton!
+        let jsonUrlString = "https://api.myjson.com/bins/gaenp"
         
-        let url = Bundle.main.url(forResource: "response", withExtension: "json")
+        var partidosServices: PartidoServiceProtocol = PartidoService()
+        lazy var partidos:[Partido] = []
+        /*
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            floatButtonScanner.layer.cornerRadius = floatButtonScanner.frame.height/2
+            loadPlaces()
+        }*/
         
-        guard let jsonData = url
-            else{
-                print("data not found")
-                return
+        func loadPartidos() {
+            partidosServices.getPartido(with: jsonUrlString) {
+                (listPartidos, error) in
+                if error != nil { // Deal with error here
+                    print("error")
+                    return
+                }else if let listPartidos = listPartidos{
+                    print("=======================================")
+                    print(listPartidos)
+                    self.partidos = listPartidos
+                    self.agregarImg()
+                }
+            }
         }
         
-        guard let data = try? Data(contentsOf: jsonData) else { return }
-        
-        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else{return}
-        
-        if let dictionary = json as? [String: Any] {
-            
-            if let title = dictionary["title"] as? String {
-                labelHeader.text = title
-            }
-            
-            if let year = dictionary["year"] as? Double {
-                print("Year is \(year)")
-            }
-            if let more_info = dictionary["more_info"] as? Double {
-                //This doesn't get printed.
-                print("More_info is \(more_info)")
-            }
-            
-            for (key, value) in dictionary {
-                print("Key is: \(key) and value is \(value)" )
-            }
-            
-        }
-        
-        //Now lets populate our TableView
-        let newUrl = Bundle.main.url(forResource: "marvel", withExtension: "json")
-        
-        guard let j = newUrl
-            else{
-                print("data not found")
-                return
-        }
-        
-        guard let d = try? Data(contentsOf: j)
-            else { print("failed")
-                return
+        func agregarImg() {
+            for index in 0...(partidos.count-1){
+                partidosServices.getImagePartido(with: partidos[index].urlFlagTeam1!) {
+                    (img, error) in
+                    if error != nil { // Deal with error here
+                        print("error")
+                        return
+                    }else if let img = img{
+                        self.partidos[index].flagTeam1 = img
+                    }
+                    //self.collectionViewMain.reloadData()
+                }
+                partidosServices.getImagePartido(with: partidos[index].urlFlagTeam2!) {
+                    (img, error) in
+                    if error != nil { // Deal with error here
+                        print("error")
+                        return
+                    }else if let img = img{
+                        self.partidos[index].flagTeam2 = img
+                    }
+                    self.collectionViewMain.reloadData()
+                }
                 
-        }
-        
-        guard let rootJSON = try? JSONSerialization.jsonObject(with: d, options: [])
-            else{ print("failedh")
-                return
-                
-        }
-        
-        if let JSON = rootJSON as? [String: Any] {
-            labelHeader.text = JSON["title"] as? String
-            
-            guard let jsonArray = JSON["movies"] as? [[String: Any]] else {
-                return
             }
             
-            print(jsonArray)
-            let name = jsonArray[0]["name"] as? String
-            print(name ?? "NA")
-            print(jsonArray.last!["year"] as? Int ?? 1970)
-            
-            movieList = jsonArray.compactMap{return MatchData($0)}
-            
-            self.tableView.reloadData()
-            
         }
         
         
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return partidos.count
+        }
         
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieList.count
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let currentMovie = movieList[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = currentMovie.movieName
-        cell.detailTextLabel?.text = "\(currentMovie.movieYear)"
-        return cell
-    }
-    
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionViewMain.dequeueReusableCell(withReuseIdentifier: "celdaMain", for: indexPath) as! MainCollectionViewCell
+            cell.team1.text = partidos[indexPath.item].team1
+            cell.team2.text = partidos[indexPath.item].team2
+            cell.score.text = partidos[indexPath.item].score
+            cell.dateMatch.text = partidos[indexPath.item].dateMatch
+            cell.imgTeam1.image = partidos[indexPath.item].flagTeam1
+            cell.imgTeam2.image = partidos[indexPath.item].flagTeam2
+            print("para cada imagen",partidos[indexPath.item].flagTeam1)
+            return cell
+        }
+        /*
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if let item = sender as? UICollectionViewCell,
+                let indexPath = collectionViewMain.indexPath(for: item),
+                let detailVC = segue.destination as? DetailViewController{
+                // detailVC.place = places[indexPath.item]
+            }
+        }*/
+
 }
-
-
-struct MatchData {
-    var team1Name: String
-    var team2Name: String
-    var score: String
-    var dateMatch: String
-    init(_ dictionary: [String: Any]) {
-        self.team1Name = dictionary["team1"] as? String ?? "NA1"
-        self.team2Name = dictionary["team2"] as? String ?? "NA2"
-        self.score = dictionary["score"] as? String ?? "0 - 0"
-        self.dateMatch = dictionary["dateMatch"] as? String ?? "dd/mm/aa"
-    }
-}
-
-
---
+++ AmericaCup/MainCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MainCollectionView.swift
+//  AmericaCup
+//
+//  Created by Herman on 7/22/19.
+//  Copyright © 2019 David Leon. All rights reserved.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var team1: UILabel!
+    @IBOutlet var team2: UILabel!
+    @IBOutlet var dateMatch: UILabel!
+    @IBOutlet var imgTeam2: UIImageView!
+    @IBOutlet var score: UILabel!   
+    @IBOutlet var imgTeam1: UIImageView!
+}
+
