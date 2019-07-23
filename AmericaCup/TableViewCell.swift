3154b7cc82218addd00e45ebb173a8c040450605
David Leon <leoc@unicauca.edu.co>
2019-07-23 00:51:30 -0500

    Last commit, app not run.

---
+++ AmericaCup/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  AmericaCup
+//
+//  Created by Herman on 7/22/19.
+//  Copyright © 2019 David Leon. All rights reserved.
+//
+
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet var team2: UILabel!
+    @IBOutlet var imgTeam2: UIImageView!
+    
+    @IBOutlet var score: UILabel!
+    @IBOutlet var team1: UILabel!
+    
+    @IBOutlet var dateMatch: UILabel!
+    
+    @IBOutlet var imgTeam1: UIImageView!
+}
